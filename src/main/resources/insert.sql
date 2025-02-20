96086223d9ba98e0fae2e4464d89c26e54bc7e04
Ракитин Леонид Павлович <lrakitin@alfabank.ru>
2025-02-20 17:19:48 +0300

    Updated inserts

--- src/main/resources/insert.sql
+++ src/main/resources/insert.sql
@@ -1,6 +1,7 @@
-INSERT INTO public.telegram_bot (assistant_id, bot_token_id, bot_type, name) VALUES ('asst_4u2rJPsHU9dJp3UdjEo1At0X', '7644505811:AAF0LJ2a7wMjXb72lGwA04lk6rirc-hmYQY', 'GODFATHER_BOT', 'UpDaily Platform');
-INSERT INTO public.heygen_data (id, avatar_id, bot_token_id, voice_id) VALUES ('f9c969f2-9188-4507-aaa0-f76d05680827', '56af96effa464cedae632dc85f8d2984', '7981707951:AAHfs0V_w5MH27rA7oCycDW0x2xc158nTCY', '671d6729b8da48aba81152cc29a631e3');
-INSERT INTO public.eleven_labs_data (id, bot_token_id, voice_id, voice_model_id) VALUES ('f9c969f2-9188-4507-aaa0-f76d05680827', '7981707951:AAHfs0V_w5MH27rA7oCycDW0x2xc158nTCY','HaBYvWY4zf3TchfK0j5Y', 'eleven_multilingual_v2');
+INSERT INTO public.telegram_bot (telegram_user, assistant_id, bot_token_id, bot_type, name, vector_store_id) VALUES (1058980949, 'asst_4u2rJPsHU9dJp3UdjEo1At0X', '7644505811:AAF0LJ2a7wMjXb72lGwA04lk6rirc-hmYQY', 'GODFATHER_BOT', 'UpDaily Platform', 'vs_67b7399b2504819181695b0001852baa');
+INSERT INTO public.telegram_bot (telegram_user, assistant_id, bot_token_id, bot_type, name, vector_store_id) VALUES (1058980949, 'asst_4u2rJPsHU9dJp3UdjEo1At0X', '6477484573:AAETV3wt5c7KTYLywleaVM2LrTbOb6lwWWU', 'CLIENT_BOT', 'Just test', 'vs_67b7399b2504819181695b0001852baa');
+INSERT INTO public.telegram_bot (telegram_user, assistant_id, bot_token_id, bot_type, name, vector_store_id) VALUES (1058980949, 'asst_4u2rJPsHU9dJp3UdjEo1At0X', '7616072522:AAHOhyrpvZsvPWcrn8nnkGxCJ1rduTqeXA8', 'CLIENT_BOT', 'UpDaily Demo Example', 'vs_nBKshcDQFWNMpwWcxRwFj3yf');
 
--- String voiceId = "HaBYvWY4zf3TchfK0j5Y";
--- String modelId = "eleven_multilingual_v2";
+INSERT INTO public.heygen_data (id, avatar_id, bot_token_id, voice_id) VALUES ('f9c969f2-9188-4507-aaa0-f76d05680827', '56af96effa464cedae632dc85f8d2984', '7616072522:AAHOhyrpvZsvPWcrn8nnkGxCJ1rduTqeXA8', '671d6729b8da48aba81152cc29a631e3');
+
+INSERT INTO public.eleven_labs_data (id, bot_token_id, voice_id, voice_model_id) VALUES ('f9c969f2-9188-4507-aaa0-f76d05680827', '7616072522:AAHOhyrpvZsvPWcrn8nnkGxCJ1rduTqeXA8', 'HaBYvWY4zf3TchfK0j5Y', 'eleven_multilingual_v2');
